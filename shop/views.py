7a79888f750916fcee7ceec35841d22aa081172a
topping <reyna11@naver.com>
2023-07-29 20:34:53 +0900

    0729_황다은 기본셋팅 및 상품 모델 추가

---
+++ shop/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, get_object_or_404
+from .models import *
+
+# Create your views here.
+def product_detail(request, id, product_slug=None):
+    product = get_object_or_404(Product, id=id, slug=product_slug)
+
+    return render(request, 'shop/detail.html', {'product':product})
