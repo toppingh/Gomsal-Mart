441cfacb8cf17d9e6cc4e0f5e5e17e0ff71ff654
hootzz <wjqwjql3@gmail.com>
2023-08-08 17:06:17 +0900

    0808_홍윤기_마이페이지

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     #path('login/', views.login, name='login'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login_page.html'), name='login'),
     path('signup/', views.signUp, name='signup'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='accounts/login_page.html'), name='logout'),
+    path('userinfo/<int:pk>/', views.userinfo, name='userinfo'),
 ]
