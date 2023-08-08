d89b5d43574319f880b47c94f068c62a20d4dad6
Hyun <121593967+cHynekim@users.noreply.github.com>
2023-08-09 01:04:30 +0900

    0809_김채현 검색바 클릭시 검색창 이동 구현
    main 페이지에 연결되는 js

---
+++ js/schScript.js
@@ -0,0 +1,32 @@
+//최근 검색 기록을 클릭 시 입력창에 자동 입력
+let history = document.querySelectorAll('.history li');
+let contents = document.querySelectorAll('.history li a');
+let date = document.querySelectorAll('.date');
+for(let i = 0; i<history.length; i++){
+    history[i].onclick = function(){
+        let getString = contents[i].innerText;
+        document.querySelector('.header_search input').value = getString;
+    }
+}
+//검색 기록 삭제
+let removeHistory = document.querySelector('.remove_history');
+removeHistory.onclick = function(){
+    for(let i = 0; i<history.length; i++){
+        contents[i].innerText = "";
+        date[i].innerText = "";
+    }
+    document.querySelector('.history ul').classList.add('hide');
+    document.querySelector('.none_history').classList.remove('hide');
+}
+//닫기 시 메인으로
+let removeSchFocus = document.querySelector('.remove_sch_focus');
+removeSchFocus.onclick = function(){
+    document.querySelector('#wrap').classList.remove('hide');
+    document.querySelector('.sch_focus').classList.add('hide');
+}
+//메인 검색창 클릭 시 포커스 이동
+let searchBar = document.querySelector('.search input');
+searchBar.onclick = function(){
+    document.querySelector('#wrap').classList.add('hide');
+    document.querySelector('.sch_focus').classList.remove('hide');
+}
