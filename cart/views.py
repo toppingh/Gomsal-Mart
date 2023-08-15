2a042f62f2c39bfe56c5cae44f58ff1f7a639daf
topping <reyna11@naver.com>
2023-08-15 17:25:18 +0900

    0815_황다은 장바구니 2차 수정(비우기, 삭제 미완료)

--- cart/views.py
+++ cart/views.py
@@ -1,37 +1,32 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.views.decorators.http import require_POST
-
-from shop.models import Product
-from .forms import AddCartForm
+from django.shortcuts import render, redirect
 from .cart import Cart
+from shop.models import Product
 
-# Create your views here.
-@require_POST
-def add(request, product_id): # 장바구니에 담기
-    cart = Cart(request) # 장바구니 객체 생성
-    product = get_object_or_404(Product, id=product_id) # 상품 정보 전달
 
-    form = AddCartForm(request.POST) # 추가되는 상품 정보느 상세 페이지나 장바구니 페이지로부터 전달되어 이 폼을 통해 만들어진 데이터이다.
-    if form.is_valid():
-        cd = form.cleaned_data
-        # 장바구니에 상품과 해당 상품의 수량을 더하고 상세 화면인지 장바구니 화면인지에 따라 방식을 다르게 한다.
-        cart.add(product=product, quantity=cd['quantity'], is_update=['is_update'])
-
-    return redirect('cart:cart') # 뷰 동작 시 장바구니 화면을 보여준다.
-
-# 장바구니에서 삭제하기
-def remove(request, product_id):
-    cart = Cart(request)
-    product = get_object_or_404(Product, id=product_id)
-    cart.remove(product)
-    return redirect('cart:cart') # 뷰 동작 시 장바구니 화면을 보여준다.
-
-# 장바구니 화면을 볼 수 있는 뷰
 def detail(request):
     cart = Cart(request)
+    cart_items = list(cart)
+    total_price = cart.get_product_total()
 
-    # 노출될 상품을 장바구니 객체인 cart로부터 가져온다.
-    for product in cart:
-        # 상품의 수량 (수정) = 폼을 이용해서 상품마다 하나씩 추가하는데 수정하는대로 반영해야 하므로 is_update=True
-        product['quantity_form'] = AddCartForm(initial={'quantity':product['quantity'], 'is_update':True})
-    return render(request, 'cart/cart.html', {'cart':cart})
+    return render(request, 'cart/detail.html', {'cart':cart_items, 'total_price':total_price})
+
+def product_add(request, product_id):
+    if request.method == "POST":
+        cart = Cart(request)
+        product = Product.objects.get(id=product_id)
+        cart.add(product=product, quantity=1, is_update=False)
+        return redirect('cart:cart')
+    else:
+        return redirect('cart:cart')
+
+def remove(request, product_id):
+    cart = Cart(request)
+    product = Product.objects.get(id=product_id)
+    cart.remove(product=product)
+
+    return redirect('cart:cart')
+
+def clear(request):
+    cart = Cart.objects.get_cart(request)
+    cart.clear(request)
+    return redirect('cart:cart')
--- static/js/cart.js
+++ static/js/cart.js
@@ -40,22 +40,32 @@ let basket = {
     },
     //화면 업데이트
     updateUI: function () {
-        document.querySelector('#sum_p_num').textContent = '상품갯수: ' + this.totalCount.formatNumber() + '개';
-        document.querySelector('#sum_p_price').textContent = '합계금액: ' + this.totalPrice.formatNumber() + '원';
+        document.querySelector('#sum_p_num').textContent = '상품갯수: ' + this.totalCount + '개';
+        document.querySelector('#sum_p_price').textContent = '합계금액: ' + this.totalPrice + '원';
     },
     //개별 수량 변경
-    changePNum: function (pos) {
+    changePNum: function (pos, action) {
         var item = document.querySelector('input[name=p_num'+pos+']');
         var p_num = parseInt(item.getAttribute('value'));
-        var newval = event.target.classList.contains('up') ? p_num+1 : event.target.classList.contains('down') ? p_num-1 : event.target.value;
-        console.log(newval);
-        if (parseInt(newval) < 1 || parseInt(newval) > 99) { return false; }
+//        var newval = event.target.classList.contains('up') ? p_num+1 : event.target.classList.contains('down') ? p_num-1 : event.target.value;
+//        console.log(newval);
+//        if (parseInt(newval) < 1 || parseInt(newval) > 99) { return false; }
+        var newval;
+        if (action === 'up') {
+            newval = p_num + 1;
+        } else if (action === 'down') {
+            newval = p_num - 1;
+        } else {
+            newval = parseInt(action);
+        }
+
+        if (newval < 1 || newval > 99) {return false;}
 
         item.setAttribute('value', newval);
         item.value = newval;
 
         var price = item.parentElement.parentElement.previousElementSibling.firstElementChild.getAttribute('value');
-        item.parentElement.parentElement.nextElementSibling.textContent = (newval * price).formatNumber()+"원";
+        item.parentElement.parentElement.nextElementSibling.textContent = (newval * price)+"원";
         //AJAX 업데이트 전송
 
         //전송 처리 결과가 성공이면
@@ -74,10 +84,10 @@ let basket = {
   }
 
   // 숫자 3자리 콤마찍기
-  Number.prototype.formatNumber = function(){
-    if(this==0) return 0;
-    let regex = /(^[+-]?\d+)(\d{3})/;
-    let nstr = (this + '');
-    while (regex.test(nstr)) nstr = nstr.replace(regex, '$1' + ',' + '$2');
-    return nstr;
-  };
+//  Number.prototype.formatNumber = function(){
+//    if(this==0) return 0;
+//    let regex = /(^[+-]?\d+)(\d{3})/;
+//    let nstr = (this + '');
+//    while (regex.test(nstr)) nstr = nstr.replace(regex, '$1' + ',' + '$2');
+//    return nstr;
+//  };
