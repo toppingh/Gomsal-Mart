5f19ae6382e0dee74b7a80acda43f5ca7c7f1275
hootzz <wjqwjql3@gmail.com>
2023-08-18 21:32:54 +0900

    0818_홍윤기_정렬

--- search/views.py
+++ search/views.py
@@ -3,10 +3,11 @@ from django.shortcuts import render, redirect
 
 from search.models import SearchHistory
 from shop.models import Product
-from django.db.models import Q
+from django.db.models import Q, Count
+
 
 # Create your views here.
-def searchResult(request):
+def SearchResult(request):
     products = None
     query = None
     if 'q' in request.GET:
@@ -19,6 +20,26 @@ def searchResult(request):
 
     return render(request, 'shop/product_list_page.html', {'query':query, 'products':products})
 
+def searchResult(request):
+    products = None
+    query = None
+    sort = request.GET.get('sort')  # 정렬 옵션 가져오기
+    if 'q' in request.GET:
+        query = request.GET.get('q')
+        products = Product.objects.all().filter(Q(name__contains=query) | Q(description__contains=query))
+
+        if sort == 'popularity':
+            products = products.annotate(num_comments=Count('comment', filter=Q(comment__rating__isnull=False))).order_by('-num_comments')
+        elif sort == 'fastest':
+            products = products.order_by('delivery_date')
+        elif sort == 'cheap':
+            products = products.order_by('price')
+
+        if request.user.is_authenticated:
+            SearchHistory.objects.create(user=request.user, query=query)
+            request.session['search_results'] = list(products.values('id', 'slug'))
+
+    return render(request, 'shop/product_list_page.html', {'query': query, 'products': products})
 
 def clear_search_history(request):
     if request.user.is_authenticated:
